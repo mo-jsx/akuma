11d025fa89ba58fcd54b307886e201296d3c33b3
mojsx <mojsx.dev@gmail.com>
2023-03-02 18:31:46 +0100

    feat: add sort data by timestamp in desc order.

---
+++ src/utils/sortByTime.test.ts
@@ -0,0 +1,15 @@
+import sortByTime from "./sortByTime";
+import generateData from "./generateData";
+import { DataProps } from "../types/DataProps";
+
+describe("sort array by time", () => {
+    const data: DataProps[] = generateData(10)
+
+    test("sort desc", () => {
+        const sortedData = data.sort(sortByTime)
+
+        for(let i = 0; i < sortedData.length - 1; i++) {
+            expect(sortedData[i].timestamp.getTime()).toBeGreaterThanOrEqual(sortedData[i+1].timestamp.getTime())
+        }
+    })
+})
---
+++ src/utils/sortByTime.ts
@@ -0,0 +1,17 @@
+import { DataProps } from "../types/DataProps"
+
+const sortByLatest = (a:DataProps, b:DataProps) => {
+    const timeA = a.timestamp;
+    const timeB = b.timestamp;
+
+    if(timeA < timeB) {
+      return 1
+    }
+    if(timeA > timeB) {
+      return -1
+    }
+
+    return 0;
+  }
+
+export default sortByLatest;
