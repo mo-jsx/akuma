a8676d129549ccb06c7efa0c4bdfb14f95c3e6a4
mojsx <mojsx.dev@gmail.com>
2023-03-02 12:32:28 +0100

    test: title is rendering.

--- src/components/Card/Card.test.tsx
+++ src/components/Card/Card.test.tsx
@@ -1,3 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import Card from "./Card";
+import { DataProps } from "../../types/DataProps";
+
 describe("Card", () => {
-    test.todo("Card is rendering");
-})
+    // let data: DataProps;
+    const data = {
+        id: 123,
+        title: "Lorem ipsum dolor sit amet.",
+        popularity: 0.1,
+        timestamp: new Date(),
+    };
+
+    test("Card is rendering", () => {
+        render(<Card data={data} />);
+        expect(screen.getByText(data.title)).toBeInTheDocument();
+    });
+});
+
+export {};
