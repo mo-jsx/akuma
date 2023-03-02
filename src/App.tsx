dbc1eb4cdc2570f6b763dbcbc22e5deb6ef5b30a
mojsx <mojsx.dev@gmail.com>
2023-03-02 12:02:40 +0100

    feat: add fetchData() to fetch and parse the .csv file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,52 @@
-import React from 'react';
+// MODULES
+import React, { useState, useEffect } from 'react';
+import * as Papa from "papaparse";
+
+// TYPES
+import { DataProps } from './types/DataProps';
+
+// LAYOUTS
 import News from "./layouts/News/News";
+
+// FILES
 import "./styles/app.css";
 
 const App = () => {
+  const [data, setData] = useState<DataProps[]>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+
+        const response = await fetch('./src/assets/newsSource.csv');
+        
+        Papa.parse(await response.text(), {
+          header: true,
+          dynamicTyping: true,
+          encoding: " ",
+          complete: result => {
+            const parsedData = result.data as DataProps[];
+            setData(parsedData);
+          },
+          error: (err: any) => {
+            console.log(err.name);
+            console.log(err.message);
+            
+          }
+        });
+      }
+      catch(err: any) {
+        console.log("AN ERROR OCCURED : ", err.name);
+        console.log(err.message);
+      }
+    }
+
+    fetchData();
+  }, []);
+  
   return (
     <div className="app">
-      <News />
+      <News data={data} />
       <div className="sidebar">Hot News</div>
     </div>
   )
