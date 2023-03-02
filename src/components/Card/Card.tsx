06e92ba171471abb72f832cdd9e8028d8fc14091
mojsx <mojsx.dev@gmail.com>
2023-03-02 11:40:25 +0100

    feat: update Card types.

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,9 +1,15 @@
+// MODULES
 import React from 'react';
+
+//TYPES
+import { DataProps } from "types/DataProps";
 import "./card.css";
 
-const Card = () => {
+const Card = (props: DataProps) => {
+    const { title } = props;
+
   return (
-    <div>Card</div>
+    <div>{title}</div>
   )
 }
 
