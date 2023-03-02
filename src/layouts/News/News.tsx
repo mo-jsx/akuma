c134e2c958aef83a9ac19edcf0ad1ab941fd033c
mojsx <mojsx.dev@gmail.com>
2023-03-02 12:04:49 +0100

    feat: add types, pass data to children components.

--- src/layouts/News/News.tsx
+++ src/layouts/News/News.tsx
@@ -1,11 +1,23 @@
+// MODULES
 import React from 'react';
+
+// TYPES 
+import { DataProps } from 'types/DataProps';
+
+// COMPONENTS
 import Card from "../../components/Card/Card";
+
+// FILES
 import "./news.css";
 
-const News = () => {
+const News = (props: { data: DataProps[]}) => {
+  const { data } = props;
+
+  const head = data[0]
+
   return (
     <div className="news">
-        <Card data={} />
+        <Card data={head} />
         <div>Feed</div>
     </div>
   )
