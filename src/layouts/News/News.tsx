94cb5d465cdc97ae601263d348ce1a476f9187b0
mojsx <mojsx.dev@gmail.com>
2023-03-02 11:24:48 +0100

    feat: import Card component.

--- src/layouts/News/News.tsx
+++ src/layouts/News/News.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Card from "../../components/Card/Card";
 import "./news.css";
 
 const News = () => {
   return (
     <div className="news">
-        <div>Card</div>
+        <Card />
         <div>Feed</div>
     </div>
   )
