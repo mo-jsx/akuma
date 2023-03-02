c10b001cd6734f08c21aec0c6ef83895ddadcb62
mojsx <mojsx.dev@gmail.com>
2023-03-02 19:47:39 +0100

    styles: add today's date, format files.

--- src/layouts/News/News.tsx
+++ src/layouts/News/News.tsx
@@ -1,9 +1,9 @@
 // MODULES
-import React from 'react';
+import React from "react";
 import sortByPopularity from "../../utils/sortByPopularity";
 
-// TYPES 
-import { DataProps } from 'types/DataProps';
+// TYPES
+import { DataProps } from "types/DataProps";
 
 // COMPONENTS
 import Card from "../../components/Card/Card";
@@ -12,19 +12,24 @@ import Feed from "../../components/Feed/Feed";
 // FILES
 import "./news.css";
 
-const News = (props: { data: DataProps[]}) => {
-  const { data } = props;
+const News = (props: { data: DataProps[] }) => {
+    const { data } = props;
 
     //Sort by popularity
-    const sortedData:DataProps[] = data.sort(sortByPopularity)
+    const sortedData: DataProps[] = data.sort(sortByPopularity);
 
     const [head, ...tail] = sortedData;
-  return (
-    <div className="news">
-        <Card data={head} />
-        <Feed data={tail} />
-    </div>
-  )
-}
 
-export default News
+    //Get today's date
+    const today = new Date().toLocaleDateString();
+
+    return (
+        <div className="news">
+            <h5 className="date">{today}</h5>
+            <Card data={head} />
+            <Feed data={tail} />
+        </div>
+    );
+};
+
+export default News;
