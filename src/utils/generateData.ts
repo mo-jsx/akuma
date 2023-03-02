e15d2050f3d0be8dace8cba06645ca19c3465a95
mojsx <mojsx.dev@gmail.com>
2023-03-02 17:37:07 +0100

    feat: add generateData for data mocking.

---
+++ src/utils/generateData.ts
@@ -0,0 +1,21 @@
+import { faker } from "@faker-js/faker";
+import { DataProps } from "../types/DataProps";
+
+const generateData = (numElement: number) => {
+    const data: DataProps[] = []
+
+    for(let i = 0; i <= numElement; i++) {
+        const row: DataProps = {
+                id: faker.datatype.number(),
+                title: faker.lorem.lines(1),
+                popularity: faker.datatype.float({ min: 0, max: 1, precision: 0.01}),
+                timestamp: faker.datatype.datetime()
+        }
+
+        data.push(row)
+    }
+
+    return data;
+}
+
+export default generateData;
