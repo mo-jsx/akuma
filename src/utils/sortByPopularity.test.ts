c68d6680f3bafe9bbfb97385d76c0e3cb5957ce3
mojsx <mojsx.dev@gmail.com>
2023-03-02 15:41:27 +0100

    feat: add sortByPopularity function.

---
+++ src/utils/sortByPopularity.test.ts
@@ -0,0 +1,43 @@
+import sortByPopularity from "./sortByPopularity";
+import { DataProps } from "../types/DataProps";
+
+describe("sort array by popularity", () => {
+    const data: DataProps[] = [
+        {
+            id: 123,
+            title: "Lorem ipsum dolor sit amet.",
+            popularity: Math.random(),
+            timestamp: new Date(),
+        },
+        {
+            id: 234,
+            title: "Lorem ipsum dolor sit amet.",
+            popularity: Math.random(),
+            timestamp: new Date(),
+        },
+        {
+            id: 345,
+            title: "Lorem ipsum dolor sit amet.",
+            popularity: Math.random(),
+            timestamp: new Date(),
+        },
+        {
+            id: 456,
+            title: "Lorem ipsum dolor sit amet.",
+            popularity: Math.random(),
+            timestamp: new Date(),
+        },
+
+    ]
+
+    console.log(data);
+    
+
+    test("sort asc", () => {
+        const sortedData = data.sort(sortByPopularity)
+
+        for(let i = 0; i < sortedData.length - 1; i++) {
+            expect(sortedData[i].popularity).toBeGreaterThanOrEqual(sortedData[i+1].popularity)
+        }
+    })
+})
