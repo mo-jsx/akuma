3d57daa4cae59360c7e2f0aee51e76cbe220da8a
mojsx <mojsx.dev@gmail.com>
2023-03-03 12:50:57 +0100

    chores: add import.

--- src/layouts/News/News.test.tsx
+++ src/layouts/News/News.test.tsx
@@ -1,4 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import News from "./News";
+import {generateData, sortByPopularity} from "../../utils/";
+import { DataProps } from "types/DataProps";
+
 describe("News", () => {
-    test.todo("News is rendering");
+    const data: DataProps[] = generateData(30);
+
+    test("News is rendering", () => {
+        render(<News data={data}/>)
+    });
+
+    // expect(screen.get)
 })
 export {};
