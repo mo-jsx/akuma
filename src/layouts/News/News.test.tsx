6bb716d0921c1f44e6698e292ff94d2bc5a99adf
mojsx <mojsx.dev@gmail.com>
2023-03-03 16:48:38 +0100

    test: add News layout integration test.

--- src/layouts/News/News.test.tsx
+++ src/layouts/News/News.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import News from "./News";
 import {generateData, sortByPopularity} from "../../utils/";
 import { DataProps } from "types/DataProps";
@@ -6,10 +6,24 @@ import { DataProps } from "types/DataProps";
 describe("News", () => {
     const data: DataProps[] = generateData(30);
 
-    test("News is rendering", () => {
-        render(<News data={data}/>)
-    });
+    test("News Image is rendering", () => {
+        render(<News data={data} />);
+        const image = screen.getByRole("img");
+        expect(image).toBeInTheDocument();
+    })
 
-    // expect(screen.get)
+
+    test("News Feed is rendering", () => {
+        render(<News data={data}/>);
+
+        const rowItems = screen.getAllByRole("listitem");        
+        
+        rowItems.forEach(row => {
+            expect(row).toBeInTheDocument();
+        });
+
+        const secondItem = screen.getByTestId(data[1].id);
+        expect(secondItem.title).toMatch(data[1].title)
+    })
 })
 export {};
